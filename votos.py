28ac197f48328f66b55687f1dc6b91a0e447b10e
nicolasss994 <chrivicen994@gmail.com>
2026-04-11 20:34:28 -0400

    Analisis de votos y elegir ganador

---
+++ votos.py
@@ -0,0 +1,20 @@
+votos = ["Ana", "Pepe", "Ana", "Luis", "Ana", "Pepe", "Luis", "Ana"]
+conteo = {}
+ganador = ""
+max_votos = 0
+
+for persona in votos:
+    if persona not in conteo:
+        conteo[persona] = 1
+    else:
+        conteo[persona] += 1
+for persona, cantidad in conteo.items():
+    if cantidad > max_votos:
+        max_votos = cantidad    
+        ganador = persona
+print("-"*15, " CANTIDAD DE VOTOS ", "-"*15)
+for persona, cantidad in sorted(conteo.items()):
+    print(f"- {persona}: {cantidad}")
+print(f"Ganador: {ganador}")
+print(conteo)
+        
